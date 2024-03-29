1ddcc43a2ff7dd9196a97c0b6736adb1ddd71410
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-03-28 21:46:15 -0300

    Exercise solution.

--- src/main/java/Hamming.java
+++ src/main/java/Hamming.java
@@ -1,9 +1,27 @@
 public class Hamming {
+    private final String leftStrand;
+    private final String rightStrand;
+    private final int hammingDistance;
+
     public Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (leftStrand.length() != rightStrand.length())
+            throw new IllegalArgumentException("strands must be of equal length");
+
+        this.leftStrand = leftStrand;
+        this.rightStrand = rightStrand;
+        hammingDistance = calculateHammingDistance();
     }
 
     public int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return hammingDistance;
+    }
+
+    private int calculateHammingDistance() {
+        if (leftStrand.length() != rightStrand.length()) return 0;
+
+        var hammingDistance = 0;
+        for (int i = 0; i < leftStrand.length(); i++)
+            if (leftStrand.charAt(i) != rightStrand.charAt(i)) hammingDistance++;
+        return hammingDistance;
     }
 }
--- src/test/java/HammingTest.java
+++ src/test/java/HammingTest.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,31 +10,26 @@ public class HammingTest {
         assertThat(new Hamming("", "").getHammingDistance()).isEqualTo(0);
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testNoDistanceBetweenShortIdenticalStrands() {
         assertThat(new Hamming("A", "A").getHammingDistance()).isEqualTo(0);
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testCompleteDistanceInSingleLetterDifferentStrands() {
         assertThat(new Hamming("G", "T").getHammingDistance()).isEqualTo(1);
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testDistanceInLongIdenticalStrands() {
         assertThat(new Hamming("GGACTGAAATCTG", "GGACTGAAATCTG").getHammingDistance()).isEqualTo(0);
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testDistanceInLongDifferentStrands() {
         assertThat(new Hamming("GGACGGATTCTG", "AGGACGGATTCT").getHammingDistance()).isEqualTo(9);
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testValidatesFirstStrandNotLonger() {
         assertThatExceptionOfType(IllegalArgumentException.class)
@@ -43,7 +37,6 @@ public class HammingTest {
                 .withMessage("strands must be of equal length");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testValidatesSecondStrandNotLonger() {
         assertThatExceptionOfType(IllegalArgumentException.class)
@@ -51,7 +44,6 @@ public class HammingTest {
                 .withMessage("strands must be of equal length");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testDisallowLeftEmptyStrand() {
         assertThatExceptionOfType(IllegalArgumentException.class)
@@ -59,7 +51,6 @@ public class HammingTest {
                 .withMessage("strands must be of equal length");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testDisallowRightEmptyStrand() {
         assertThatExceptionOfType(IllegalArgumentException.class)
